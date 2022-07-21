8299e4607b561cb241d41ddeed9aacbdcec8d99d
AnierinB <anierin@evolution-x.org>
2022-07-21 18:03:42 +0000

    kebab: Configure OPlusExtras device specific features
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Change-Id: Ic8b474e93c4d0c5c025991bada2f7a9cc77b5b78

--- device.mk
+++ device.mk
@@ -29,6 +29,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 PRODUCT_PACKAGES += \
+    OPlusExtrasResTarget \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget \
     OPlusSystemUIResTarget
